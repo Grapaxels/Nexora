8f435e9a9964007dd56cd6512d0d9c189b1b0403
Nikhil <nikhilkumar1338@gmail.com>
2026-09-11 16:45:16 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
     port: 5173,
     strictPort: true,
     proxy: {
-      "/api": "http://127.0.0.1:3001",
+      "/api": "https://nexora.grapaxels.in/",
     },
   },
 });
